a700c622957b4679da292bd8debc33a51fb0fe8c
Conny Gu <connygy@googlemail.com>
2020-01-03 01:55:27 +0100

    158. Starting the Burger Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
-import Layout from './components/Layout/Layout';
+import Layout from "./components/Layout/Layout";
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import BurgerBuilder from "./components/BurgerBuilder/BurgerBuilder";
 
 export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <BurgerBuilder></BurgerBuilder>
-        </Layout>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Layout>
+					<BurgerBuilder></BurgerBuilder>
+				</Layout>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Auxilian";
-
+import Burger from "../../components/Burger/Burger";
 export class BurgerBuilder extends Component {
 	render() {
 		return (
 			<Aux>
-				<div>Burger</div>
+				<Burger></Burger>
 				<div>Build Controls</div>
 			</Aux>
 		);
