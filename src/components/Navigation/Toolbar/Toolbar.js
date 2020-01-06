e726a0be8611e7ce3a386ccc4a9d41c5a8ca69d9
Conny Gu <connygy@googlemail.com>
2020-01-06 01:55:43 +0100

    Working on Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,9 @@ const toolbar = props => {
 	return (
 		<header className={classes.Toolbar}>
 			<div>MENU</div>
-			<Logo>LOGO</Logo>
+			<div className={classes.Logo}>
+				<Logo>LOGO</Logo>
+			</div>
 			<NavigationItems></NavigationItems>
 		</header>
 	);
