0473978c42dd5af36d5152f7770ee953bb3df078
Conny Gu <connygy@googlemail.com>
2020-01-11 02:16:26 +0100

    207-211. Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDINT_PRICES = {
 	salad: 0.5,
@@ -37,7 +38,30 @@ class BurgerBuilder extends Component {
 	};
 
 	purchasingContinueHandler = () => {
-		alert("You will continue...");
+		// alert("You will continue...");
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: "Max Schwarzmüller",
+				address: {
+					street: "Teststreet 1",
+					zipCode: "41351",
+					country: "Germany"
+				},
+				email: "test@test.com"
+			},
+			deliveryMethod: "fastest"
+		};
+
+		axios
+			.post("/orders.json/", order)
+			.then(response => {
+				console.log(response);
+			})
+			.catch(error => {
+				console.log(error);
+			});
 	};
 	updatePurchaseState = updatedIngredients => {
 		const sum = Object.keys(updatedIngredients)
