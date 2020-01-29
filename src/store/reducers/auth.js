950d6acb48d174402cd17e5238ac913146dc6495
Conny Gu <connygy@googlemail.com>
2020-01-29 23:22:21 +0100

    360. Logging Users Out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -28,6 +28,12 @@ const authFail = (state, action) => {
 	});
 };
 
+const authLogout = (state, action) => {
+    return updateObject(state, {token: null, userId: null
+        
+    })
+}
+
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.AUTH_START:
@@ -35,7 +41,10 @@ const reducer = (state = initialState, action) => {
 		case actionTypes.AUTH_SUCCESS:
 			return authSuccess(state, action);
 		case actionTypes.AUTH_FAIL:
-			return authFail(state, action);
+            return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT:
+            return authLogout(state, action)
+
 		default:
 			return state;
 	}
