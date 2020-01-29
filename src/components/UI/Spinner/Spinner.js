0f8a92c456905f81966e96e060a6c5635beacce9
Conny Gu <connygy@googlemail.com>
2020-01-29 20:59:10 +0100

    359. Adding a Spinner

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,6 +1,9 @@
 import React from "react";
 import classes from "./Spinner.module.css";
 
+/**
+ * A Spinner when lodaing. No parameter required
+ */
 const spinner = () => {
 	return <div className={classes.Loader}>Loading...</div>;
 };
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,7 +4,7 @@ import Button from "../../components/UI/Button/Button";
 import classes from "./Auth.module.css";
 import * as actions from "../../store/actions/index";
 import { connect } from "react-redux";
-
+import Spinner from "../../components/UI/Spinner/Spinner";
 export class Auth extends Component {
 	state = {
 		controls: {
@@ -89,7 +89,7 @@ export class Auth extends Component {
 		event.preventDefault();
 		this.props.onAuth(
 			this.state.controls.email.value,
-			this.state.controls.password.value, 
+			this.state.controls.password.value,
 			this.state.isSignup
 		);
 	};
@@ -109,7 +109,7 @@ export class Auth extends Component {
 			});
 		}
 
-		const form = formElementsArray.map(formElement => (
+		let form = formElementsArray.map(formElement => (
 			<Input
 				key={formElement.id}
 				elementType={formElement.config.elementType}
@@ -122,8 +122,19 @@ export class Auth extends Component {
 			/>
 		));
 
+		if (this.props.loading) {
+			form = <Spinner />;
+		}
+
+		let errorMessage = null;
+
+		if (this.props.error) {
+			errorMessage = <p>{this.props.error.message}</p>;
+		}
+
 		return (
 			<div className={classes.Auth}>
+				{errorMessage}
 				<form onSubmit={this.submitHandler}>
 					{form}
 					<Button btnType="Success">SUBMIT</Button>
@@ -136,6 +147,13 @@ export class Auth extends Component {
 	}
 }
 
+const mapStateToProps = state => {
+	return {
+		loading: state.auth.loading,
+		error: state.auth.error
+	};
+};
+
 const mapDispatchToProps = dispatch => {
 	return {
 		onAuth: (email, password, isSignup) =>
@@ -143,4 +161,4 @@ const mapDispatchToProps = dispatch => {
 	};
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
