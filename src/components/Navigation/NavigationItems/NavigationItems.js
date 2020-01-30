458acfcab0b1ba911e48a33f3521a9a41a4e2bb2
Conny Gu <connygy@googlemail.com>
2020-01-30 01:58:17 +0100

    363. Updating the UI Depending on Auth State

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,17 +2,20 @@ import React from "react";
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems = () => {
+const navigationItems = props => {
 	return (
 		<ul className={classes.NavigationItems}>
 			<NavigationItem link="/" exact={true}>
 				Burger Builder
 			</NavigationItem>
 			<NavigationItem link="/orders">Orders</NavigationItem>
-			<NavigationItem link="/auth">Authenticate</NavigationItem>
+			{props.isAuthenticated ? (
+				<NavigationItem link="/logout">Logout</NavigationItem>
+			) : (
+				<NavigationItem link="/auth">Authenticate</NavigationItem>
+			)}
 		</ul>
 	);
 };
 
 export default navigationItems;
- 
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -3,6 +3,7 @@ import Aux from "../Auxilian";
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import {connect} from 'react-redux'
 
 class Layout extends Component {
 	state = {
@@ -20,10 +21,12 @@ class Layout extends Component {
 	};
 
 	render() {
+		console.log(this.props.isAuthenticated)
 		return (
 			<Aux>
-				<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+				<Toolbar isAuth={this.props.isAuthenticated}drawerToggleClicked={this.sideDrawerToggleHandler} />
 				<SideDrawer
+					isAuth={this.props.isAuthenticated}
 					open={this.state.showSideDrawer}
 					closed={this.sideDrawerClosedHandler}
 				/>
@@ -33,4 +36,9 @@ class Layout extends Component {
 	}
 }
 
-export default Layout;
+const mapStateToProps = state => {
+	return { 
+		isAuthenticated: state.auth.token !== null
+}}
+
+export default connect(mapStateToProps)(Layout);
