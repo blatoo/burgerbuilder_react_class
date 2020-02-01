757da55fab3289dd90706e2f53865bc93ad98bf6
Conny Gu <connygy@googlemail.com>
2020-02-01 18:36:48 +0100

    379. Adding Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
 import { connect } from "react-redux";
+import Auth from "./containers/Auth/Auth";
+
+const Checkout = React.lazy(() => import("./containers/Checkout/Checkout"));
+const Orders = React.lazy(() => import("./containers/Orders/Orders"));
+
 export class App extends Component {
 	componentDidMount() {
 		this.props.onTryAutoSignup();
@@ -19,17 +21,31 @@ export class App extends Component {
 				<Route path="/auth" component={Auth} />
 				<Route path="/" exact component={BurgerBuilder} />
 				<Redirect to="/" />
-				
 			</Switch>
 		);
 
 		if (this.props.isAuthenticated) {
 			routes = (
 				<Switch>
-					<Route path="/checkout" component={Checkout} />
-					<Route path="/orders" component={Orders} />
-					<Route path="/logout" component={Logout} />
+					<Route
+						path="/checkout"
+						render={props => (
+							<Suspense fallback={<div>Loading Checkout...</div>}>
+								<Checkout {...props} />
+							</Suspense>
+						)}
+					/>
+					<Route
+						path="/orders"
+						render={() => (
+							<Suspense fallback={<div>Loading Orders...</div>}>
+								<Orders />
+							</Suspense>
+						)}
+					/>
 					<Route path="/auth" component={Auth} />
+
+					<Route path="/logout" component={Logout} />
 					<Route path="/" exact component={BurgerBuilder} />
 				</Switch>
 			);
@@ -37,9 +53,7 @@ export class App extends Component {
 
 		return (
 			<div>
-				<Layout>
-					{routes}
-				</Layout>
+				<Layout>{routes}</Layout>
 			</div>
 		);
 	}
@@ -58,4 +72,5 @@ const mapDispatchToProps = dispatch => {
 		}
 	};
 };
+
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
